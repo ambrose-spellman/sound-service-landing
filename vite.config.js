706b8132bf44fa9340d3f700a6a653aab53f1cfb
Saikal <saikal0515@gmail.com>
2025-01-18 23:50:22 +0500

    Deploy to github pages

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,22 @@
 import { defineConfig } from "vite";
+import { resolve } from 'path';
 
 export default defineConfig({
   build: {
     rollupOptions: {
       input: {
-        main: "index.html",
-        contacts: "contacts.html",
-        modal1: "modal1.html",
-        modal2: "modal2.html",
+        main: resolve(__dirname, 'index.html'),
+        contacts: resolve(__dirname, 'contacts.html'),
+        modal1: resolve(__dirname, 'modal1.html'),
+        modal2: resolve(__dirname, 'modal2.html'),
       },
     },
+    assetsInlineLimit: 0,
+    outDir: 'dist',
+    emptyOutDir: true,
+    // assetsDir: 'assets',
   },
+  publicDir: 'public',
+  base: './',
+ 
 });
