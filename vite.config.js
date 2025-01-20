5776e24028e44ea177448f473436e8c8b1142f55
Saikal <saikal0515@gmail.com>
2025-01-20 15:27:27 +0500

    fixes vite

--- vite.config.js
+++ vite.config.js
@@ -11,6 +11,9 @@ export default defineConfig({
         modal1: resolve(__dirname, 'modal1.html'),
         modal2: resolve(__dirname, 'modal2.html'),
       },
+      output:{
+        assetFileNames: 'assets/[name].[ext]'
+      }
     },
     assetsInlineLimit: 0,
     outDir: 'dist',
@@ -18,12 +21,21 @@ export default defineConfig({
     // assetsDir: 'assets',
   },
   publicDir: 'public',
-  base: '/sound-service-landing/',
+  base: './',
   plugins: [
     viteStaticCopy({
       targets: [
         { src: 'CNAME', dest: '' } // Adjust the path if CNAME is in a different location
       ],
-    })
+    }),
+    {
+      name: 'html-transform',
+      transformIndexHtml(html) {
+        // Transform link and script tags to use relative paths
+        return html
+          .replace(/(href|src)="\//g, '$1=".')
+          .replace(/(href|src)="\.\/assets/g, '$1="assets');
+      }
+    }
   ]
 });
